bee3b64c0263d51feaadc22ab72416b3a1348b35
elena <329655021@qq.com>
2017-10-11 16:44:34 +0800

    cloudCompute shop

--- src/components/compute/Shop.vue
+++ src/components/compute/Shop.vue
@@ -12,23 +12,24 @@
     <div class="miner_list">
       <div class="box">
         <div class="sort">
-          <div class="item" @click="setSort('all')">默认</div>
-          <div class="item" v-for="s,k in sort" @click="setSort(k)">{{s.title}}</div>
+          <div class="item" @click="setSort('all',$event)">默认</div>
+          <div :class="['item', {active: edit==k}, {up: !s.value}]" v-for="s,k in sort" @click="setSort(k,$event)">{{s.title}}<span class="iconfont"></span></div>
         </div>
         <div class="data">
           <div class="item" v-for="d,k in computeDate">
-            <h3>{{d.title}}</h3>
+            <h3>{{d.title}}<span class="icon_img"></span></h3>
             <div class="info_box">
-              <div class="info" v-for="n,i in dataNav">
-                <div class="info_item">
-                  <div>
+              <template v-for="n,i in dataNav">
+                <div class="info">
+                  <div class="text">
                     <span class="num">{{d[i]}}</span>
                     <span>{{n.unit}}</span>
                   </div>
                   <p>{{n.title}}</p>
                 </div>
                 <div class="line"></div>
-              </div>
+              </template>
+              <router-link class="btn" :to="'/cloudCompute/detail/'+d.id">立即购买</router-link>
             </div>
           </div>
         </div>
@@ -44,13 +45,20 @@
         nav: ['BTC挖矿', 'BCC挖矿', 'ETH挖矿', 'ETC挖矿'],
         sort: [{title: '价格', option: ['price_asc', 'price_desc'], value: 0}, {title: '算力', option: ['base_asc', 'base_desc'], value: 0}, {title: '出售总数', option: ['num_asc', 'num_desc'], value: 0}],
         computeDate: [{id: 1, title: '阿瓦隆001号', type: 'btc', price: 10000, hash: '9.0', number: 500, leftNum: 199}, {id: 1, title: '阿瓦隆001号', type: 'btc', price: 10000, hash: '9.0', number: 500, leftNum: 199}, {id: 1, title: '阿瓦隆001号', type: 'btc', price: 10000, hash: '9.0', number: 500, leftNum: 199}, {id: 1, title: '阿瓦隆001号', type: 'btc', price: 10000, hash: '9.0', number: 500, leftNum: 199}, {id: 1, title: '阿瓦隆001号', type: 'btc', price: 10000, hash: '9.0', number: 500, leftNum: 199}, {id: 1, title: '阿瓦隆001号', type: 'btc', price: 10000, hash: '9.0', number: 500, leftNum: 199}],
-        dataNav: {'price': {title: '每台服务器价格', unit: '元'}, 'hash': {title: '每台服务器算力', unit: 'T'}, 'number': {title: '出售服务器总数', unit: '台'}, 'leftNum': {title: '剩余可售服务器', unit: '台'}}
+        dataNav: {'price': {title: '每台服务器价格', unit: '元'}, 'hash': {title: '每台服务器算力', unit: 'T'}, 'number': {title: '出售服务器总数', unit: '台'}, 'leftNum': {title: '剩余可售服务器', unit: '台'}},
+        edit: -1
       }
     },
     methods: {
-      setSort (n) {
+      setSort (n, e) {
+        this.edit = n
         var obj = this.sort[n]
         var str = ''
+        for (let ele of this.sort) {
+          if (obj !== ele) {
+            ele.value = 0
+          }
+        }
         if (obj) {
           obj.value = +(!obj.value)
           str = obj.option[obj.value]
@@ -119,32 +127,90 @@
       margin-top:-20px;
       background: #f7f8fa;
       padding-top:50px;
+      padding-bottom:50px;
       .box{
         @include main
         .sort{
           background: $white;
           padding:10px 25px;
+          color:$light_text;
           @include flex
           border: 1px solid $border;
           .item{
             cursor: pointer;
-            padding:10px 30px;
+            padding:0 30px;
+            line-height: 40px;
             background: #f7f8fa;
             & + .item{
               margin-left:50px
+            }
+            .iconfont{
+              @include block(24)
+              vertical-align: middle;
+              transition:all .3s;
+              &:before{
+                font-size: 24px;
+                content:'\e60e'
+              }
+            }
+            &.active{
+              color:$blue
+            }
+            &.up{
+              .iconfont{
+                transform:rotate(180deg)
+              }
             }
           }
         }
         .data{
           .item{
+            padding:30px 50px;
             background: $white;
             h3{
               font-size: 18px;
+              margin-bottom:10px;
+              .icon_img{
+                @include block(18)
+                margin-left:5px;
+                background: url('../../assets/images/css_sprites.png') -152px -209px;
+              }
             }
             .info_box{
               @include flex(space-between)
               .info{
-                
+                .text .num{
+                  font-size: 24px;
+                }
+                &:first-child{
+                  .text{
+                    color: $orange
+                  }
+                }
+                p{
+                  color: $light_black;
+                }
+              }
+              .line{
+                width:1px;
+                height: 50px;
+                background: $border
+              }
+              .btn{
+                width:145px;
+                color: $orange;
+                text-align: center;
+                line-height: 48px;
+                border-radius:5px;
+              }
+            }
+            &:not(:last-child){
+              margin-bottom:10px;
+            }
+            &:hover{
+              background: #f4f4f4;
+              .btn{
+                @include button($orange)
               }
             }
           }
