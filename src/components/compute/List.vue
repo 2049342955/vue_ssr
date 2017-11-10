c7f95ae51f6d767ae80063b43a0f6a4975a868f4
FuLiKai <695257958@qq.com>
2017-11-10 19:56:42 +0800

    活动

--- src/components/compute/List.vue
+++ src/components/compute/List.vue
@@ -22,20 +22,20 @@
         </ul>
         <ul class="tbody">
           <li v-for="n,k in reslist">
-            <span>{{n.name}}</span>
-            <span>{{n.num}}</span>
-            <span>{{n.price}} 元</span>
-            <span>{{n.time}}</span>
+            <span>{{n.mobile|format}}</span>
+            <span>{{n.ammount}} 台</span>
+            <span>{{n.pay_value}} 元</span>
+            <span>{{n.buy_time.split(" ")[0]}} {{n.buy_time.split(" ")[1].split("-")[0]}} : {{n.buy_time.split(" ")[1].split("-")[1]}} : {{n.buy_time.split(" ")[1].split("-")[2]}}</span>
             <span>{{n.status}}</span>
           </li>
         </ul>
       </div>
     </div>
     <div class="info">
-      <div class="mainone" v-for="n, k in info">
+      <!-- <div class="mainone" v-for="n, k in info">
         <div class="left">
           <img src="../../assets/images/header.png" class="header"/>
-          <!-- <img src="../../assets/images/color.png" class="index"/> -->
+           <img src="../../assets/images/color.png" class="index"/> 
           <h5>{{n.name}}</h5>
           <p>{{n.time}}</p>
         </div>
@@ -44,20 +44,53 @@
       <div class="button">
         <button style="margin-right:7px;"><img src="../../assets/images/leftjian.png" class="trans"/></button>
         <button><img src="../../assets/images/leftjian.png"/></button>
-      </div> 
+      </div>  -->
+      <div class="opacity" style="width:1070px;overflow:hidden;margin-left:50px;">
+        <el-carousel :interval="5000" arrow="always" height="500px">
+          <el-carousel-item class="mainone" v-for="n, k in info" :key="item">
+            <div class="left">
+              <img src="../../assets/images/header.png" class="header"/>
+              <img src="../../assets/images/color.png" class="index"/> 
+              <h5>{{n.name}}</h5>
+              <p>{{n.time}}</p>
+            </div>
+            <div class="right">{{n.content}}</div>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  import api from '../../util/function'
+  import util from '../../util'
+  import { mapState } from 'vuex'
   export default {
     data () {
       return {
         item: [{left: '规模化部署，专业的散热设备，远离运行噪音，使用低价合规电。', con: '运行', right: '在家运行占空间，又会产生大量的噪音和热量，家用电的成本也是不小的开支。'}, {left: '基础设施全方位提供服务。', con: '配套', right: '需要自己购买专用电源、控制组件和矿机支架等。'}, {left: 'T专业人员进行配置、维护。', con: '软件', right: '组装矿机后需要专业的软件支持，对于新人需要付出一定的学习成本。'}, {left: '出现问题平台负责解决，并安排专业人员进行维修。', con: '维修', right: '一旦矿机出现问题，需要自行解决维修问题，挖矿停止，将会造成一定的损失。'}],
         res: ['用户名', '购买数量', '购买金额', '购买时间', '状态'],
-        reslist: [{name: '15570150173', num: '12台', price: '2259986.00', time: '2017-11-11 08:30:87', status: '抢购成功'}, {name: '15570150173', num: '12台', price: '2259986.00元', time: '2017-11-11 08:30:87', status: '抢购成功'}, {name: '15570150173', num: '12台', price: '2259986.00元', time: '2017-11-11 08:30:87', status: '抢购成功'}, {name: '15570150173', num: '12台', price: '2259986.00元', time: '2017-11-11 08:30:87', status: '抢购成功'}, {name: '15570150173', num: '12台', price: '2259986.00元', time: '2017-11-11 08:30:87', status: '抢购成功'}],
-        info: [{url: '../../assets/images/header.png', name: '高新技术主管', time: 'Shsx jsxk ', content: ' 比特币的价格今年迄今为止的涨幅已达600%以上，目前的交易价格接近每枚7200美元，今后可能会涨至8000美元。但这将是它最后一个高点，至少一段时间内是如此。比特币自突破6044美元以来，显示出强劲的涨势。下一重点关注7941美元，再进一步走高前可能在该水平整固。”'}]
+        reslist: [],
+        pay_value: '',
+        info: [{url: '../../assets/images/header.png', name: '高新技术主管', time: 'Shsx jsxk ', content: ' 比特币的价格今年迄今为止的涨幅已达600%以上，目前的交易价格接近每枚7200美元，今后可能会涨至8000美元。但这将是它最后一个高点，至少一段时间内是如此。比特币自突破6044美元以来，显示出强劲的涨势。下一重点关注7941美元，再进一步走高前可能在该水平整固。”'}, {url: '../../assets/images/header.png', name: '高新技术主管', time: 'Shsx jsxk ', content: ' 比特币的价格今年迄今为止的涨幅已达600%以上，目前的交易价格接近每枚7200美元，今后可能会涨至8000美元。但这将是它最后一个高点，至少一段时间内是如此。比特币自突破6044美元以来，显示出强劲的涨势。下一重点关注7941美元，再进一步走高前可能在该水平整固。”'}]
       }
+    },
+    mounted () {
+      var self = this
+      util.post('showBuyList', {sign: api.serialize({token: this.token})}).then(function (res) {
+        api.checkAjax(self, res, () => {
+          self.reslist = res
+        })
+      })
+    },
+    computed: {
+      ...mapState({
+        token: state => state.info.token
+      })
+    },
+    filters: {
+      format: api.telReadable
     }
   }
 </script>
@@ -220,7 +253,7 @@
       .left{
         position: absolute;
         top:66px;
-        left:112px;
+        left:62px;
         width: 160px;
         text-align: center;
         .index{
@@ -252,7 +285,7 @@
         width: 740px;
         height: 117px;
         position: absolute;
-        right: 100px;
+        right: 50px;
         top:157px;
         color: white;
         font-size: 15px;
@@ -262,24 +295,32 @@
         position: absolute;
         right: 100px;
         bottom: 63px;
-        button{
-          width: 30px;
-          height: 40px;
-          border-radius: 3px;
-          background: #6151ae;
-          border:0;
-          img{
-            width: 10px;
-            height: 11px;
-            position: relative;
-            top: -3px;
-          }
-        }
         .trans{
           transform: rotate(180deg);
-          
         }
       }
     }
+  }
+  .el-carousel__arrow--left{
+    width: 30px;
+    height: 40px;
+    border-radius: 3px;
+    background: #6151ae;
+    border:0;
+    background-size: 10px 11px;
+    position: relative;
+    margin-left:910px;
+    margin-top: 60px;
+  }
+  .el-carousel__arrow--right{
+    width: 30px;
+    height: 40px;
+    border-radius: 3px;
+    background: #6151ae;
+    border:0;
+    background-size: 10px 11px;
+    position: relative;
+    margin-left:50px;
+    margin-top: 60px;
   }
 </style>
