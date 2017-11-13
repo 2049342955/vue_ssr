726f342dd4ff2e8a6e8ac39352bd8b6958058848
FuLiKai <695257958@qq.com>
2017-11-13 12:15:10 +0800

    修改

---
+++ src/components/mobile/MbdcForm.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="mask">
+    <button>NEIVNEI</button>
+  </section>
+</template>
+<style type="text/css" lang="scss">
+  @import '../../assets/css/style.scss';
+  .mask{
+    
+  }
+</style>
