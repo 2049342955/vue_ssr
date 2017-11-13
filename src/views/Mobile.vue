93f4a1b28d7b4046732577df9be3c5a5b3fd3313
elena <329655021@qq.com>
2017-11-13 11:30:55 +0800

    优化

---
+++ src/views/Mobile.vue
@@ -0,0 +1,12 @@
+<template>
+  <article class="mobile">
+    <router-view class="content"></router-view>
+  </article>
+</template>
+
+<style type="text/css" lang="scss">
+  @import '../assets/css/style.scss';
+  .mobile{
+    
+  }
+</style>
