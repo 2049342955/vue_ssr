00bf8d86cf69514dc295ec4e3c1afad006616470
FuLiKai <695257958@qq.com>
2017-11-13 16:05:42 +0800

    视频

--- src/components/mobile/Mnotice.vue
+++ src/components/mobile/Mnotice.vue
@@ -1,91 +1,61 @@
 <template>
   <section class="mask">
-    <div class="form_box">
-      <div class="close" @click="$parent.closeEdit()">
-        <span class="icon"  style="position: absolute;top: 45px;right: 20px;"></span>
-      </div>
-      <h2>{{title}}</h2>
-      <form :class="['form form_content', {card_form: $parent.edit==='card'}]" @submit.prevent="$parent.submit" novalidate v-if="!contract">
-        <FormField :form="form"></FormField>
-        <p v-if="$parent.fee&&$parent.edit!=='GetIncome'">手续费：{{$parent.total_price * $parent.fee|format}}元<span class="fee">({{$parent.fee*100+'%'}})</span></p>
-        <p v-if="$parent.fee&&$parent.edit==='GetIncome'">手续费：{{$parent.total_price * $parent.fee|format(8)}}btc<span class="fee">({{$parent.fee*100+'%'}})</span></p>
-        <button name="btn">确认提交</button>
-      </form>
-      <div class="contract" v-html="contract" v-else></div>
+    <div class="list">
+      <router-link to="#" class="route">
+        <span class="left">
+          <i>算力网正式上线</i>
+          <em>2015.10.22 17:14:25</em>
+        </span>
+        <span class="right">加上你看下三星陕西安康三姓家奴刷卡机想那些实习就阿克苏新年开始向纳税是希纳斯希纳斯</span>
+      </router-link>
     </div>
   </section>
 </template>
 
 <script>
-  import FormField from '@/components/common/FormField'
-  import api from '@/util/function'
   export default {
-    components: {
-      FormField
-    },
-    props: {
-      form: {
-        type: Array
-      },
-      title: {
-        type: String
-      },
-      contract: {
-        type: String
-      }
-    },
-    filters: {
-      format: api.decimal
-    }
   }
 </script>
 
 <style type="text/css" lang="scss">
-  @import '../../assets/css/style.scss';
-  .mask{
-    @include mask
-    .form_box{
-      .form_content{
-        padding:40px 130px;
-        @include form(v)
-        .input{
-          .select{
-            @include flex(space-between)
-            select{
-              width:33%;
-              height:32px;
-              line-height: 32px;
-              border:none;
-            }
-          }
+  .list{
+    width: 100%;
+    height: 100vh;
+    background: #f5f5f9 !important;
+    .route{
+      width: 100%;
+      height: 3rem;
+      display: block;
+      background: white;
+      border-top:1px solid #dddddd;
+      padding:0 .5rem;
+      border-bottom:1px solid #dddddd;
+      box-sizing: border-box;
+      margin-top: 0.5rem;
+      .left{
+        width: 100%;
+        display: flex;
+        justify-content:space-between;
+        padding-top: 0.5rem;
+        i{
+          font-size: 0.6rem;
+          color: black;
         }
-        &.card_form .input{
-          span{
-            &:first-child {
-              width: 120px;
-              text-align: right;
-            }
-            &:nth-child(2) {
-              left: 135px
-            }
-          }
-          input,.sel,.select{
-            padding-left:185px
-          }
-        }
-        .fee{
-          font-size: 12px;
-          color: $light_black;
-          margin-left:5px
+        em{
+          font-style: normal;
+          color: #b8b8b8;
+          font-size: 0.4rem;
         }
       }
-    }
-    .contract{
-      padding:30px;
-      height:680px;
-      overflow:auto;
-      p, p > *{
-        white-space:wrap !important;
+      .right{
+        display: inline-block;
+        width: 100%;
+        white-space: nowrap;  
+        text-overflow:ellipsis; 
+        overflow:hidden;
+        color: #666666;
+        font-size: 0.5rem;
+        padding-top: .35rem;
       }
     }
   }
