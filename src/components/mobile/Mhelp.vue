3f150e2f9c161f620a6b7d238e2d2ab681bd9f07
FuLiKai <695257958@qq.com>
2017-11-14 20:31:24 +0800

    修改

--- src/components/mobile/Mhelp.vue
+++ src/components/mobile/Mhelp.vue
@@ -4,17 +4,19 @@
       <span class="left">< <em>返回</em></span>
       <span>帮助中心</span>
     </div>
-    <div class="issues_box">
+    <div class="issues_box" v-show="show">
       <div class="issues_lists">
         <div class="item" v-for="n,k in nav" @click="fetchData(n.help_class_id,k)">
           <p class="titleall">{{n.name}}<em>></em></p>
           <div class="issues_list" v-if="num===k">
-            <a class="item" v-for="l,k in list">
+            <a class="item" v-for="l,k in list" @click="detailcontent(l.id)">
               <span>{{l.title}}</span>
             </a>
           </div>
         </div>
       </div>
+    </div>
+    <div class="content" v-show="!show" v-html="nowItem.content">
     </div>
   </section>
 </template>
@@ -29,7 +31,8 @@
       return {
         nav: [],
         list: [],
-        nowItem: []
+        nowItem: [],
+        show: true
       }
     },
     methods: {
@@ -51,10 +54,17 @@
           this.$store.commit('SET_NUM', k)
           eles[k].className = 'item active'
         }, 0)
+      },
+      detailcontent (id) {
+        var self = this
+        var helpid = id
+        this.show = false
+        util.post('getHelpContent', {sign: api.serialize({token: this.token, help_id: helpid})}).then(function (res) {
+          self.nowItem = res
+        })
       }
     },
     mounted () {
-      console.log(this.num)
       var self = this
       util.post('getHelpClass', {sign: api.serialize({token: this.token})}).then(function (res) {
         self.nav = res
@@ -75,6 +85,22 @@
   margin:0;
 }
   .issues{
+    .content{
+            width: 100%;
+            overflow: hidden;
+            height: 100vh;
+            padding:0.5rem;
+            background: #f5f5f9;
+            box-sizing: border-box;
+            text-indent:0rem !important;
+            img{
+              width: 100%;
+              height: auto;
+            }
+            span{
+              text-indent:0rem !important;
+            }
+          }
     .issues_box{
       background: #f5f5f9;
       width: 100%;
@@ -137,17 +163,6 @@
             color:#327fff;
           }
         }
-        .content{
-            width: 100%;
-            overflow: hidden;
-            padding:0 .5rem;
-            background: #f5f5f9;
-            box-sizing: border-box;
-            box-sizing: border-box;
-            img{
-              width: 80%;
-            }
-          }
       }
     }
   }
