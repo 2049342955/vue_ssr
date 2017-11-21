9e971ba5869a94c6f069422e2243417b9e301e90
FuLiKai <695257958@qq.com>
2017-11-21 11:46:22 +0800

    实名

--- src/components/common/Product.vue
+++ src/components/common/Product.vue
@@ -196,7 +196,7 @@
         }
         if (!(this.true_name && this.true_name.status === 1)) {
           api.tips('请先实名认证', () => {
-            if (api.checkEquipment) {
+            if (this.$route.path.includes('mHome')) {
               this.$router.push({name: 'madministration'})
             } else {
               this.$router.push({name: 'account'})
@@ -206,7 +206,7 @@
         }
         if (!(this.bank_card && this.bank_card.status === 1)) {
           api.tips('请先绑定银行卡', () => {
-            if (api.checkEquipment) {
+            if (this.$route.path.includes('mHome')) {
               this.$router.push({name: 'madministration'})
             } else {
               this.$router.push({name: 'account'})
