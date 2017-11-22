98aa4ade8d4945a3ceab0a7a75932bb617bbf21a
elena <329655021@qq.com>
2017-11-22 18:51:31 +0800

    跳转修改

--- src/components/home/DataList.vue
+++ src/components/home/DataList.vue
@@ -78,8 +78,7 @@
         }
         if (!(this.true_name && this.true_name.status === 1)) {
           api.tips('请先实名认证', () => {
-            // console.log(api.checkEquipment)
-            if (this.$route.path.includes('mHome')) {
+            if (this.isMobile) {
               this.$router.push({name: 'madministration'})
             } else {
               this.$router.push({name: 'account'})
@@ -89,8 +88,7 @@
         }
         if (!(this.bank_card && this.bank_card.status === 1)) {
           api.tips('请先绑定银行卡', () => {
-            // console.log(api.checkEquipment)
-            if (this.$route.path.includes('mHome')) {
+            if (this.isMobile) {
               this.$router.push({name: 'madministration'})
             } else {
               this.$router.push({name: 'account'})
@@ -122,7 +120,8 @@
       ...mapState({
         token: state => state.info.token,
         true_name: state => state.info.true_name,
-        bank_card: state => state.info.bank_card
+        bank_card: state => state.info.bank_card,
+        isMobile: state => state.isMobile
       })
     }
   }
